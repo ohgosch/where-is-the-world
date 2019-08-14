7276b7b4e0a9b1a37f1fe1433d15a19822815453
Gosch <pro@gosch.io>
2019-08-14 19:21:05 -0300

    Add new rule to isObject method

--- src/logics/utils/object.js
+++ src/logics/utils/object.js
@@ -1,4 +1,4 @@
-export const isObject = (object) => !!object && typeof object === 'object';
+export const isObject = (object) => !!object && typeof object === 'object' && object.constructor === Object;
 
 export const isFilled = (object) => isObject(object) && !!Object.keys(object).length;
 
